2192091fb792d5f679cc67b74386724b65faf8bd
Shobhit Jain <91949960+shobhitjain936@users.noreply.github.com>
2023-07-31 01:35:46 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,19 @@
+public class age {
+    public static void main(String[] args){
+        int age=9;
+    if(age<18 && age<=60)
+    {
+        
+        if(age<10)
+        {
+            System.out.println("kido");
+        }
+        else{
+        System.out.println("user is not valid");
+    }
+}
+    else{
+        System.out.println("user is valid");
+    }
+}
+}
